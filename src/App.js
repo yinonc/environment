891e6ac441653db6dae1b0d46ebf68c55644ce08
Yinon Cohen <yinonc@wix.com>
2019-11-29 13:03:14 +0200

    adding eslint and husky

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from 'react'
 import './app.scss'
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+	constructor(props) {
+		super(props)
+	}
 
-    render() {
-        return (
-            <div className="whole-app">
-                Environment App
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="whole-app">
+				<span>Environment App</span>
+			</div>
+		)
+	}
 }
 
 export default App
